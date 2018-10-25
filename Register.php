718dfece9a860a9fee137d66f809ac51cbcdc722
JacekWy <wypartj@gmail.com>
2018-10-25 22:02:22 +0200

    a

--- Register.php
+++ Register.php
@@ -5,11 +5,26 @@
  * Date: 25.10.2018
  * Time: 21:04
  */
-
+require_once ("Connect_MySql.php");
 
 $user = $_POST["Username"];
 $password = $_POST["Password"];
 $passwordr = $_POST["RPassword"];
 $city = $_POST["City"];
+
+
+
+if($connect->connect_error)
+{
+    die("Connection failed: " . $connect->connect_error);
+} else
+{
+    $connect->query("insert into medival.login (User, Password,RPassword,City) values ('$user','$password','$passwordr','$city')");
+
+    $connect->close();
+    header('Location: Index.html');
+
+}
+
 
 
