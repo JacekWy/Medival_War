2481c8ad1d3db77e60c1102edf58e5b20e8d186f
JacekWy <wypartj@gmail.com>
2018-10-25 21:07:31 +0200

    a

--- LoginCheck.php
+++ LoginCheck.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jack
- * Date: 25.10.2018
- * Time: 18:50
- */
 
 $user = $_POST["Username"];
 $password = $_POST["Password"];
 
 
-echo $user;
---
+++ Register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jack
+ * Date: 25.10.2018
+ * Time: 21:04
+ */
+
+
+$user = $_POST["Username"];
+$password = $_POST["Password"];
+$passwordr = $_POST["RPassword"];
+
+
+echo $user;
