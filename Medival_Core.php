149f181c5f9c797fe8094a5848469b7b65ae5e68
JacekWy <wypartj@gmail.com>
2018-10-26 15:39:12 +0200

    a

--- Medival_Core.php
+++ Medival_Core.php
@@ -14,7 +14,7 @@ echo $user."</br>";
 
 $result = $connect->query("select * from medival.materials as mat join medival.login as log on  mat.id = log.id where log.User = '$user'");
 
-$wood = $connect->query("select * from medival.login as log join medival.sawmill as saw on log.Sawmill_LVL = saw.LVL where log.User = '$user'");
+$wood = $connect->query("select saw.Prod from medival.login as log join medival.sawmill as saw on log.Sawmill_LVL = saw.LVL where log.User = '$user'");
 
 
 if ($result->num_rows > 0){
@@ -24,12 +24,10 @@ if ($result->num_rows > 0){
         echo "Food: ".$row['Food']."</br>";
         echo "People: ".$row['People']."</br>";
         echo "City: ".$row['City']."</br>";
-        echo "Sawmill LVL: ".$row['Sawmill_LVL'];
+        echo "Sawmill LVL: ".$row['Sawmill_LVL']."</br>";
     }
 }
 
-if($wood->num_rows > 0){
-    while($roww = $wood->fetch_assoc()){
-        echo "Produkcja wood". $roww['Prod'];
-    }
-}
+$roww = $wood->fetch_row();
+echo "Produkcja drzewa/min: ".$roww[0];
+
