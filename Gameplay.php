4075cd37ca2df6568353d0ef153a9a3e152ab584
JacekWy <wypartj@gmail.com>
2018-10-26 15:30:57 +0200

    a

---
+++ Gameplay.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jack
+ * Date: 26.10.2018
+ * Time: 14:53
+ */
+
+require_once ("Connect_MySql.php");
+
+
--- Medival_Core.php
+++ Medival_Core.php
@@ -14,11 +14,22 @@ echo $user."</br>";
 
 $result = $connect->query("select * from medival.materials as mat join medival.login as log on  mat.id = log.id where log.User = '$user'");
 
+$wood = $connect->query("select * from medival.login as log join medival.sawmill as saw on log.Sawmill_LVL = saw.LVL where log.User = '$user'");
+
+
 if ($result->num_rows > 0){
     while ($row = $result->fetch_assoc()){
         echo "Wood: ". $row['Wood']."</br>";
         echo "Stone: ".$row['Stone']."</br>";
         echo "Food: ".$row['Food']."</br>";
         echo "People: ".$row['People']."</br>";
+        echo "City: ".$row['City']."</br>";
+        echo "Sawmill LVL: ".$row['Sawmill_LVL'];
+    }
+}
+
+if($wood->num_rows > 0){
+    while($roww = $wood->fetch_assoc()){
+        echo "Produkcja wood". $roww['Prod'];
     }
 }
