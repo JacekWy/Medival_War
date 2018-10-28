0d29f8a1dae1452f7c05ffa9a4354134c9e8cf87
JacekWy <wypartj@gmail.com>
2018-10-28 12:41:15 +0100

    a

--- Gameplay.php
+++ Gameplay.php
@@ -15,13 +15,15 @@ $user = $connect->query("select * from medival.login");
 if($user->num_rows > 0 ){
     while ($row = $user->fetch_row()){
         $userr = $row[1];
+        $id = $row[0];
         echo $userr;
+        echo $id;
         $wood_prod = $connect->query("select saw.Prod from medival.login as log join medival.sawmill as saw on log.Sawmill_LVL = saw.LVL where log.User = '$userr'");
         $row_wood = $wood_prod->fetch_row();
         $up_wood = $row_wood[0];
         echo $up_wood;
 
-        $connect->query("update medival.materials set wood = wood + '$up_wood' where id in(SELECT id from login where User = '$userr')");
+        $connect->query("update medival.materials set wood = wood + '$up_wood' where id = '$id'");
 
 
     }
--- Register.php
+++ Register.php
@@ -18,7 +18,7 @@ if($connect->connect_error)
     die("Connection failed: " . $connect->connect_error);
 } else
 {
-    $connect->query("insert into medival.login (User, Password,RPassword,City) values ('$user','$password','$passwordr','$city')");
+    $connect->query("insert into medival.login (User, Password,RPassword,City,Sawmill_LVL) values ('$user','$password','$passwordr','$city',1)");
     $connect->query("insert into medival.materials (Wood,Stone,Food,People) values ('100','100','100','10')");
 
     $connect->close();
